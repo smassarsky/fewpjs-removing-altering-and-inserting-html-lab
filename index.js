5e42a5b623db798810b2d0becd18c2b5ef7b8213
smassarsky <seth.massarsky@gmail.com>
2021-01-07 15:12:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.querySelector('#main').remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerHTML = "YOUR-NAME is the champion"
